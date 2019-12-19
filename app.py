5eaaf1682353504128e6fd071265bdbcef335d48
Daniel <djumelle@oldwestbury.edu>
2019-12-19 01:19:18 -0500

    added basic soundcloud functionality

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, request, send_file
+from flask import Flask, request, send_file, jsonify, Response
+from flask_cors import CORS
+import json
+from bs4 import BeautifulSoup
+from selenium import webdriver
 import subprocess
 import os
 path = os.path.dirname(os.path.abspath(__file__))
 songPath = os.path.join(path, 'songs')
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/')
@@ -17,8 +22,6 @@ def youtube(videoId):
     print('yt downloading')
     fileName = videoId + '.mp3'
     filePath = os.path.join(path, 'songs', fileName)
-    print(filePath)
-    print(os.getcwd())
     url = 'https://www.youtube.com/watch?v=' + videoId
     subprocess.call(
         ['youtube-dl', '--extract-audio', '--audio-format', 'mp3', '-o' + songPath + '/%(id)s.%(ext)s', url], shell=False)
@@ -33,6 +36,29 @@ def spotify(type, url):
     subprocess.call(['spotdl', '-s open.spotify.com/' +
                      type + '/' + url, '-f' + songPath, '-ff', '{isrc}'], shell=False)
     return send_file(filePath)
+
+
+@app.route('/soundcloud/info')
+def soundcloudInfo():
+    term = request.args.get('q')
+    url = 'https://soundcloud.com/search?q=' + term
+    driver = webdriver.Chrome(path + '/chromedriver')
+    driver.get(url)
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    items = soup.findAll("div", {"class": "sound__body"})
+    results = []
+    for x in items:
+        results.append(str(x))
+    return Response(json.dumps(results),  mimetype='application/json')
+
+
+@app.route('/soundcloud')
+def soundcloud():
+    print('sc downloading')
+    subprocess.call(
+        ['youtube-dl', '--extract-audio', '--audio-format', 'mp3', '-o' + songPath + '/%(title)s.%(ext)s', 'https://soundcloud.com/jahkoy/letitbe'], shell=False)
+    return 'hi'
 
 
 if __name__ == '__main__':
