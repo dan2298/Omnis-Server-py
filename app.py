9ae5a62f4ab100294e06aaa8a8e2894922e258d6
Daniel <djumelle@oldwestbury.edu>
2019-12-19 19:05:00 -0500

    removed console logs and changed wait condition

--- app.py
+++ app.py
@@ -53,19 +53,19 @@ def soundcloud():
 def soundcloudInfo():
     term = request.args.get('q')
     url = 'https://soundcloud.com/search?q=' + term
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--no-sandbox')
-    driver = webdriver.Chrome(executable_path=os.environ.get(
-        "CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-    # driver = webdriver.Chrome('./chromedriver') //offline use
+    # chrome_options = webdriver.ChromeOptions()
+    # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    # chrome_options.add_argument('--headless')
+    # chrome_options.add_argument('--disable-gpu')
+    # chrome_options.add_argument('--no-sandbox')
+    # driver = webdriver.Chrome(executable_path=os.environ.get(
+    #     "CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    driver = webdriver.Chrome('./chromedriver') // offline use
     driver.get(url)
     try:
         element = WebDriverWait(driver, 15).until(
             EC.visibility_of_element_located(
-                (By.CLASS_NAME, "searchList__item"))
+                (By.CLASS_NAME, "sound__coverArt"))
         )
         html = driver.page_source
     finally:
@@ -77,8 +77,6 @@ def soundcloudInfo():
         "div", {"class": "soundTitle__usernameTitleContainer"})
     picsArr = []
     contentArr = []
-    print(len(pics))
-    print(len(content))
     for x in range(5):
         contentArr.append(str(content[x]))
         picsArr.append(str(pics[x]))
