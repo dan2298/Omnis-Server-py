456990fc51254be782c6bee60b39f1b97d824e51
Daniel <djumelle@oldwestbury.edu>
2019-12-20 21:47:07 -0500

    forgot to change chrome path

--- app.py
+++ app.py
@@ -57,14 +57,14 @@ def soundcloud(artist, song):
 def soundcloudInfo():
     term = str(urllib.parse.quote(request.args.get('q')))
     url = 'https://soundcloud.com/search/sounds?q=' + term
-    # chrome_options = webdriver.ChromeOptions()
-    # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    # chrome_options.add_argument('--headless')
-    # chrome_options.add_argument('--disable-gpu')
-    # chrome_options.add_argument('--no-sandbox')
-    # driver = webdriver.Chrome(executable_path=os.environ.get(
-    # "CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-    driver = webdriver.Chrome('./chromedriver')  # offline use
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--no-sandbox')
+    driver = webdriver.Chrome(executable_path=os.environ.get(
+        "CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    # driver = webdriver.Chrome('./chromedriver')  # offline use
     driver.get(url)
     try:
         element = WebDriverWait(driver, 15).until(
